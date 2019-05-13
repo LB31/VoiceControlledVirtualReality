34e49fe30008f7022dfefc35019b18af570499c8
Leonid <leonidb93@gmail.com>
2019-05-13 03:27:00 +0200

    change ui

--- Assets/_Scripts/SpeechMenu.cs
+++ Assets/_Scripts/SpeechMenu.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class SpeechMenu : MonoBehaviour
 {
-    
+
+    [SerializeField]
+    GameObject MenuObject;
+
+    [SerializeField]
+    string[] PossibleMenuCommands;
+
+    private bool FoundMenuCommand;
+
     void Start()
     {
+        MenuObject.SetActive(false);
+        PossibleMenuCommands = new string[] { "menu", "help", "pause", "stop" };
         // Registrate this class to get notified, when the user entered a message
         SpeechDecoder.speechDecoder.CommandTransmitter += ToggleMenu;
     }
@@ -18,11 +28,16 @@ public class SpeechMenu : MonoBehaviour
     }
 
     void ToggleMenu(string command) {
+        FoundMenuCommand = SpeechDecoder.speechDecoder.FindCommand(command, PossibleMenuCommands);
         /* TODO:
          * Open or close menu according to its current state
          * Stop the rest of the game expect the head rotation
          * Make the background behind the canvas black and white
             */
+        if (FoundMenuCommand) {
+            SpeechDecoder.speechDecoder.CommandWasFound = true;
+            MenuObject.SetActive(true);
+        }
     }
 
 
